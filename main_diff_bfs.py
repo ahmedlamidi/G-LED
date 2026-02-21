0629f44da1bdd8cc76f193b068c54b45fe8edd4e
Ahmed Lamidi <ahmedlamidi@usf.edu>
2026-02-21 03:30:44 -0500

    Save changes

--- main_diff_bfs.py
+++ main_diff_bfs.py
@@ -30,7 +30,7 @@ class Args:
 		for finding the dynamics dir
 		"""
 		self.parser.add_argument("--bfs_dynamic_folder", 
-								 default='output/feb_19_720_816_model',
+								 default='output/feb_19_feather_model_720_820',
 								 help='all the information of ks training')
 		"""
 		for diffusion model
