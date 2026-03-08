17a008a585aa7459004d194f02daf9e2a2e484b4
Ahmed Lamidi <ahmedlamidi@usf.edu>
2026-03-08 16:47:07 -0400

    Add support for smaller condition compared to Label

--- main_diff_bfs.py
+++ main_diff_bfs.py
@@ -30,7 +30,7 @@ class Args:
 		for finding the dynamics dir
 		"""
 		self.parser.add_argument("--bfs_dynamic_folder", 
-								 default='output/mar_8_horizontal_PIML_step_4',
+								 default='output/mar_8_horizontal_step_4',
 								 help='all the information of ks training')
 		"""
 		for diffusion model
--- train_model.sh
+++ train_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name=ddpm-chest
-#SBATCH -p general
+#SBATCH -p Quick
 #SBATCH --cpus-per-task=2
-#SBATCH --time=168:00:00
-#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:A40:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ahmedlamidi@usf.edu
 #SBATCH --mem=64G
